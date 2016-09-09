7139fa8be3207593495a04caabdb2b9c4134a0c9
nhoang53 <he_llo1002@yahoo.com>
2016-09-08 17:01:05 -0700

    Design and Create class

--- app/src/main/java/edu/orangecoastcollege/cs273/tipcalculator/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/tipcalculator/MainActivity.java
@@ -1,13 +1,65 @@
 package edu.orangecoastcollege.cs273.tipcalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Associate the controller with the needed views
+    private EditText amountEditText;
+    private TextView amountTextView;
+    private TextView tipTextView;
+    private TextView tipAmountTextView;
+    private TextView totalAmountTextView;
+    private SeekBar percentTipSeekBar;
+
+    // Associate the controller with the needed model
+    RestaurantBill currentBill = new RestaurantBill();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Connect the controller with the widgets in our app
+        amountEditText = (EditText) findViewById(R.id.amountEditText);
+        amountTextView = (TextView) findViewById(R.id.amountTextView);
+        tipTextView = (TextView) findViewById(R.id.tipAmountTextView);
+        tipAmountTextView = (TextView) findViewById(R.id.tipAmountTextView);
+        totalAmountTextView = (TextView) findViewById(R.id.totalAmountTextView);
+        percentTipSeekBar = (SeekBar) findViewById(R.id.percentTipsSeekBar);
+
+        // Define a listener of the amountEditText (onTextChanged)
+        amountEditText.addTextChangedListener(amountTextChangedListener); // any name here
+    }
+
+    private TextWatcher amountTextChangedListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            // Do nothing
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            // Try to get the amount from amountEditText
+            try{
+                double amount = Double.parseDouble(charSequence.toString()) / 100.0;
+                currentBill.setmAmount(amount);
+            }
+            catch (NumberFormatException e)
+            {
+                amountEditText.setText("");
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            // Do nothing
+        }
     }
 }
---
+++ app/src/main/java/edu/orangecoastcollege/cs273/tipcalculator/RestaurantBill.java
@@ -0,0 +1,65 @@
+package edu.orangecoastcollege.cs273.tipcalculator;
+
+/**
+ * Calculate a mount of Restaurant bill
+ * Created by nhoang53 on 9/8/2016.
+ */
+public class RestaurantBill {
+    private double mAmount;
+    private double mTipPercent;
+    private double mTipAmount;
+    private double mTotalAmount;
+
+    public RestaurantBill()
+    {
+        mAmount = 0.0;
+        mTipPercent = 0.0;
+        mTotalAmount = 0.0;
+        mTotalAmount = 0.0;
+    }
+
+    public RestaurantBill(double mAmount, double mTipPercent) {
+        this.mAmount = mAmount;
+        this.mTipPercent = mTipPercent;
+        recalculateAmount();
+    }
+
+    public double getmAmount() {
+        return mAmount;
+    }
+
+    public double getmTipPercent() {
+        return mTipPercent;
+    }
+
+    public double getMtipAmount() {
+        return mTipAmount;
+    }
+
+    public double getmTotalAmount() {
+        return mTotalAmount;
+    }
+
+    public void setmAmount(double mAmount) {
+        this.mAmount = mAmount;
+    }
+
+    public void setmTipPercent(double mTipPercent) {
+        this.mTipPercent = mTipPercent;
+        recalculateAmount();
+    }
+
+    public void setmTipAmount(double mtipAmount) {
+        this.mTipAmount = mtipAmount;
+    }
+
+    public void setmTotalAmount(double mTotalAmount) {
+        this.mTotalAmount = mTotalAmount;
+    }
+
+    public void recalculateAmount()
+    {
+        mTipAmount = mAmount * mTipAmount;
+        mTotalAmount = mTipAmount + mAmount;
+    }
+}
